1896cba2761886ff286439955253faf91a9a89b4
Jane Slagle <91845271+janeslagle@users.noreply.github.com>
2025-02-08 23:49:23 -0800

    Update EdgeGraphAdapter.java

--- EdgeGraphAdapter.java
+++ EdgeGraphAdapter.java
@@ -7,39 +7,129 @@ public class EdgeGraphAdapter implements EdgeGraph {
     public EdgeGraphAdapter(Graph g) { this.g = g; }
 
     public boolean addEdge(Edge e) {
-	     throw new UnsupportedOperationException();
+        String srcNode = e.getSrc();
+        boolean srcNodeAdded = g.addNode(srcNode);
+
+        String dstNode = e.getDst();
+        boolean dstNodeAdded = g.addNode(dstNode);
+
+        boolean edgeAdded = g.addEdge(srcNode, dstNode);
+
+        // nodes not addded separately from edges so can return any of these to find out if edge was actually added or not
+        return edgeAdded || srcNodeAdded || dstNodeAdded;
     }
 
     public boolean hasNode(String n) {
-	     throw new UnsupportedOperationException();
+        return g.hasNode(n);
     }
 
     public boolean hasEdge(Edge e) {
-	     throw new UnsupportedOperationException();
+        return g.hasEdge(e.getSrc(), e.getDst());
     }
 
     public boolean removeEdge(Edge e) {
-	     throw new UnsupportedOperationException();
+        if (!hasEdge(e)) {
+            return false;
+        }
+
+        String srcNode = e.getSrc();
+        String dstNode = e.getDst();
+
+        g.removeEdge(srcNode, dstNode);
+
+        if (g.succ(srcNode).isEmpty()) {
+            g.removeNode(srcNode);
+        }
+
+        if (g.pred(srcNode).isEmpty()) {
+            g.removeNode(srcNode);
+        }
+
+        if (g.succ(dstNode).isEmpty()) {
+            g.removeNode(dstNode);
+        }
+
+        if (g.pred(dstNode).isEmpty()) {
+            g.removeNode(dstNode);
+        }
+
+        return true;
     }
 
     public List<Edge> outEdges(String n) {
-      throw new UnsupportedOperationException();
+        List<Edge> outEdges = new ArrayList<>();
+
+        List<String> successors = g.succ(n);
+
+        for (String succNode: successors) {
+            outEdges.add(new Edge(n, succNode));
+        }
+
+        return outEdges;
     }
 
     public List<Edge> inEdges(String n) {
-      throw new UnsupportedOperationException();
+        List<Edge> inEdges = new ArrayList<>();
+
+        List<String> predecessors = g.pred(n);
+
+        for (String predNode: predecessors) {
+            inEdges.add(new Edge(predNode, n));
+        }
+
+        return inEdges;
     }
 
     public List<Edge> edges() {
-      throw new UnsupportedOperationException();
+        List<Edge> allEdges = new ArrayList<>();
+
+        for (String n: g.nodes()) {
+            List<Edge> nOutEdges = outEdges(n);
+
+            for (Edge e: nOutEdges) {
+                allEdges.add(e);
+            }
+        }
+
+        return allEdges;
     }
 
     public EdgeGraph union(EdgeGraph g) {
-      throw new UnsupportedOperationException();
+        EdgeGraphAdapter unionedGraph = new EdgeGraphAdapter(new ListGraph());
+
+        List<Edge> thisGraphEdges = this.edges();
+        for (Edge e: thisGraphEdges) {
+            unionedGraph.addEdge(e);
+        }
+
+        List<Edge> gGraphEdges = g.edges();
+        for (Edge e: gGraphEdges) {
+            unionedGraph.addEdge(e);
+        }
+
+        return unionedGraph;
     }
 
     public boolean hasPath(List<Edge> e) {
-	     throw new UnsupportedOperationException();
-    }
+        if (e.isEmpty()) {
+            return true;
+        }
 
+        for (Edge edge: e) {
+            if (!this.hasEdge(edge)) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < e.size() - 1; i++) {
+            Edge currEdge = e.get(i);
+            Edge nextEdge = e.get(i + 1);
+
+            if (!currEdge.getDst().equals(nextEdge.getSrc())) {
+                throw new BadPath("Given edges do not form a valid path.");
+            }
+        }
+
+        return true;
+    }
 }
